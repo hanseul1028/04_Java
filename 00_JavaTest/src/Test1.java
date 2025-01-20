8e541f6baf1532283e49c34f99279a89d265a583
hanesul1028 <khs1028ryu@naver.com>
2025-01-20 11:35:37 +0900

    java initailized

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,18 @@
+// JS와 주석 동일
+
+// RUN 버튼 클릭 시(단축키 ctrl + f11)
+// 1.자동을 컴파일 수행(javac ooo.java) -> .class 파일 생성(byte code) 생성
+// 2. 컴파일 완료 후 .class 파일 실행(java class 파일명)
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이클립스 실행 중");
+		System.out.println("타자 연습하기..");
+		
+
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,13 @@
+public class JavaTest{
+
+  public static void main(String[] args){
+    
+       System.out.println("Hello World!!!");
+       System.out.println("안녕하세요");
+
+      for(int i = 1 ; i <=5 ; i++){
+        System.out.println(i + "번째 출력");
+      }  
+}
+
+}
